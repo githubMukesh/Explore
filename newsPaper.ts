04cc5fa66fbc9b0476160c5c2bb00277c2dbcca3
githubMukesh <getmkr@hotmail.com>
2020-08-19 22:39:19 +0530

    Added UI for NewsPaper

--- newsPaper.ts
+++ newsPaper.ts
@@ -8,28 +8,39 @@ const hindustanTime = (news) => { console.log(`%c HindustanTimes \n\n\n ${news}`
 
 const theHindu = (news) => { console.log(`%cThe Hindu \n\n\n${news}`,"color: black;font-weight: bold;padding:1px")};
 
-newsPaper.subscribe(hindustanTime);
-newsPaper.subscribe(theHindu);
+// newsPaper.subscribe(hindustanTime);
+// newsPaper.subscribe(theHindu);
 
 newsPaper.next("india Win the all world cup in all sports of the word");
 
-(function(){
+(() =>{
   let hindustanTimesButton = document.createElement('button');
   hindustanTimesButton.innerHTML = 'HindustanTimes';
   hindustanTimesButton.style.margin = '10px';
-  hindustanTimesButton.onclick = hindustanTime;
+  hindustanTimesButton.onclick = () => {newsPaper.subscribe(hindustanTime);};
 
   let hinduButton = document.createElement('button');
   hinduButton.innerHTML = 'The Hindu';
-  hinduButton.onclick = theHindu;
+  hinduButton.onclick = () => {newsPaper.subscribe(theHindu);};
+
+  let unSubscribehindustanTimes = document.createElement('button');
+  unSubscribehindustanTimes.innerHTML = 'UnSubscribe HindustanTimes';
+  unSubscribehindustanTimes.style.margin = '10px';
+  unSubscribehindustanTimes.onclick = () => {newsPaper.unsubscribe(hindustanTime);};
+
+  let unSubscribeHindu = document.createElement('button');
+  unSubscribeHindu.innerHTML = 'UnSubscribe The Hindu';
+  unSubscribeHindu.onclick = () => {newsPaper.unsubscribe(theHindu);};
 
   let inputText = document.createElement('input');
 //  inputText.innerHTML = 'Send News';
   inputText.type = "text";
-  inputText.setAttribute('type', 'text');
-  //inputText.onclick = theHindu;
+  inputText.onchange = (event: any) => {newsPaper.next(event.target.value)};
 
   document.body.appendChild(hindustanTimesButton);
   document.body.appendChild(hinduButton);
-
+  document.body.appendChild(unSubscribehindustanTimes);
+  document.body.appendChild(unSubscribeHindu);
+   document.body.appendChild(inputText);
 })();
+
